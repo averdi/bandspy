c67045b35ddcb660cf3b52410ea49e96386deb69
Jeremy Garza <rangerjdg@gmail.com>
2016-03-22 15:58:52 -0500

    update

--- BandSpy/app/models/event.rb
+++ BandSpy/app/models/event.rb
@@ -2,10 +2,4 @@ class Event < ActiveRecord::Base
   belongs_to :artist
   belongs_to :venue
 
-  @events = Bandsintown::Event.search({
-      :location => 'Austin, TX',
-      :start_date => Time.now,
-      :end_date => 1.week.from_now
-    })
-
 end
