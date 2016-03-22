2fca2052ad78ae1698c075ca4e97ccf8a72aa777
Ryan Jones <ryandjones14@gmail.com>
2016-03-22 10:45:17 -0500

    added The Killers to seed.rb

--- BandSpy/db/seeds.rb
+++ BandSpy/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+ Bandsintown.app_id = 'bandspy'
+
+artist = Bandsintown::Artist.new({
+  :name => "The Killers"
+})
+Artist.create(name: artist.name, on_tour: true)
