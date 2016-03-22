14e57176399c44da00066dd816632c136cd4e0d0
Ryan Jones <ryandjones14@gmail.com>
2016-03-22 14:53:42 -0500

    user can edit profile

--- BandSpy/app/controllers/artists_controller.rb
+++ BandSpy/app/controllers/artists_controller.rb
@@ -9,10 +9,10 @@ class ArtistsController < ApplicationController
     @artists = Artist.all
 
     @events = Bandsintown::Event.search({
-:location => 'Austin, TX',
-:start_date => Time.now,
-:end_date => 1.week.from_now
-})
+      :location => 'Austin, TX',
+      :start_date => Time.now,
+      :end_date => 1.week.from_now
+    })
 
   end
 
